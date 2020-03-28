4b5d514064868948551ff925d3914eace34e07d4
Danillo <danillo.yamagush@gmail.com>
2020-03-28 19:00:42 -0300

    Criado controller ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,8 @@
 const express = require('express');
-
+const OngController = require('./controllers/OngController');
 const routes = express.Router();
 
-routes.post("/users", (request, response) =>{
-    return response.json({
-        evento: 'Semana OmniStack 11.0',
-        alunos: 'Danillo'
-    });
-});
+routes.get('/ongs', OngController.index);
+routes.post("/ongs", OngController.create);
 
 module.exports = routes;
